ec36cb214536f9450b31b54fbd14becd6e9145b6
MCENDROWSKI <MCENDROWSKI@QG273317.qg.com>
2015-09-16 16:26:23 -0500

    repaired main class

--- src/lab1/StartUp.java
+++ src/lab1/StartUp.java
@@ -5,6 +5,8 @@
  */
 package lab1;
 
+import java.util.Scanner;
+
 /**
  *
  * @author MCENDROWSKI
@@ -44,6 +46,7 @@ public class StartUp {
     public static void main(String[] args) {
 showNumbersOfCourses();
 showCourseNames();
+//lab2();
 
     }
     
@@ -71,6 +74,44 @@ showCourseNames();
         System.out.println("All names of programming courses:");
         for (ProgrammingCourse course : courses) {
             System.out.println(course.getCourseNumber());
+        }
+    }
+    
+    public static void lab2(){
+               String courseNumber;
+        ProgrammingCourse[] courses = new ProgrammingCourse[3];
+    
+
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.println("Input number of Advanced Java Course:");
+        courseNumber = keyboard.nextLine();        
+        AdvancedJavaCourse advJava = new AdvancedJavaCourse("Advance Java",courseNumber);
+        advJava.setCredits(4);
+        advJava.setPrerequisites("Yes");
+        
+        System.out.println("Input number of Intro Java Course:");
+        courseNumber = keyboard.nextLine();        
+        IntroJavaCourse introJava = new IntroJavaCourse("Intro Java",courseNumber);
+        introJava.setCredits(4);
+        introJava.setPrerequisites("Yes");
+        
+        System.out.println("Input number of Intro To Programming Course:");
+        courseNumber = keyboard.nextLine();        
+        IntroToProgrammingCourse introToProg = new IntroToProgrammingCourse("Intro To Programming",courseNumber);
+        introToProg.setCredits(3);
+        
+        
+        System.out.println("First course:\t"+advJava.getCourseName()+"\t"+advJava.getCourseNumber()+"\t"+4+"\t"+advJava.getPrerequisites());
+        System.out.println("Second course:\t"+"NO ACCESS"+"\t"+introJava.getCourseNumber()+"\t"+introJava.getCredits()+"\t"+introJava.getPrerequisites());
+        System.out.println("Third course:\t"+"Intro To Programming"+"\t"+"NO ACCESS"+"\t"+introToProg.getCredits()+"\t"+"NO PREREQUISITES");
+
+   courses[0] = new AdvancedJavaCourse(advJava.getCourseName(),advJava.getCourseNumber());
+   courses[1] = new IntroJavaCourse("Intro Java",introJava.getCourseNumber());
+   courses[1] = new IntroToProgrammingCourse("Intro To Programming",introToProg.getCourseNumber());
+   
+        for (ProgrammingCourse course : courses) {
+            course.getCourseNumber();
         }
     }
 }
